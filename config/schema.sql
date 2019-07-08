9b5ab9c65e90f5265c88295b120704690bd0eade
Daniels@dual4t.com <daniels@dual4t.com>
2019-07-08 11:06:04 -0700

    schema

--- config/schema.sql
+++ config/schema.sql
@@ -1,12 +1,17 @@
-DROP DATABASE IF EXISTS wishes_db;
+DROP DATABASE IF EXISTS users_db;
 
-CREATE DATABASE wishes_db;
+CREATE DATABASE users_db;
 
-USE wishes_db;
+USE users_db;
 
-CREATE TABLE wishes(
+CREATE TABLE Users(
     id INTEGER AUTO_INCREMENT,
-    wish VARCHAR(100) NOT NULL,
+    full_name VARCHAR(100) NOT NULL,
+    birthday INTEGER(32) NOT NULL,
+    secret VARCHAR(256) NOT NULL,
+    Arcade_token INTEGER(10) NOT NULL,
+    Raffle_ticket INTEGER(10) NOT NULL,
+    game_stats_array = VARCHAR(256)
     PRIMARY KEY (id)
 )
 
