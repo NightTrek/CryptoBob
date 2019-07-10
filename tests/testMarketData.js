b3a441df493f8cc31dba3027a611b066491c816d
Daniels@dual4t.com <daniels@dual4t.com>
2019-07-09 22:44:15 -0700

    added try catch

--- tests/testMarketData.js
+++ tests/testMarketData.js
@@ -4,7 +4,13 @@ let test = async function() {
     // let tickerIDPair = await md.storeCurrencyList();
     // console.log(tickerIDPair);
     let test = setInterval(async function(){
+        try{
         let res = await md.storeLiveCurrencyData();
+        }
+        catch(err){
+            console.log("Test Function failed ")
+            throw err;
+        }
     }, 30000);
     
 
