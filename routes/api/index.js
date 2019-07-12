b50c4ea8741d6fcbd71b686074c2129864d83773
Daniels@dual4t.com <daniels@dual4t.com>
2019-07-12 14:25:28 -0700

    fixed async functions

--- routes/api/index.js
+++ routes/api/index.js
@@ -7,48 +7,7 @@ const loginkeys = require('../../config/loginKeys');
 
 // /api/
 
-router.get("/login", function(req, res) {
-
-// //password/userame route
-// router.post("/api/marketDataforToken", async function (req, res) {
-//     console.log("POST REQ");
-//     // console.log(req.body.data.ticker)
-//     try{
-//         if(md.key[req.body.data.password] !== undefined){
-//     let connection = await sql.GetConnection()
-//     let tokenData = await sql.SelectAllAndOrderByTmestamp(connection, md.key[req.body.data.password], req.body.data.market );
-//     if(tokenData){
-//         connection.end();
-//         res.json(tokenData);
-//     }
-//     }else{
-//         throw "error 404"
-//     }
-//     }
-//     catch(err){
-//         console.log(err);
-
-//     }
-    
-// });
-
-
-
-
-
-// router.get("/", function (req, res) {
-
-   // connection.query("SELECT * FROM mining;", function(err, data) {
-    //   if (err) { 
-    //     return res.status(500).end();
-    //   }
-
-    //   res.render("index", { quotes: data });
-    // });
-    res.send("/api/")
-});
-
-router.post("/login", function (req, res) {
+router.post("/login", async function (req, res) {
     try{
     let connection = await sql.GetConnection();
     let passwordHash = await sql.selectSomethingWhere(connection, password, users, userName, req.body.data.userName);
@@ -63,7 +22,7 @@ router.post("/login", function (req, res) {
 }
 });
 
-router.post("/signup", function (req, res) {
+router.post("/signup", async function (req, res) {
     try{
     let hash = await  bcrypt.hash(req.body.data.password, 10);
    let signUpData = {
