44098979f22b475019f5cb7e336211d2ef428fa1
Garrett Young <garrettyoung510@gmail.com>
2019-07-13 15:12:39 -0700

    added notifications page and test route for notifications

--- routes/html/index.js
+++ routes/html/index.js
@@ -3,23 +3,11 @@ const sql = require('../../controlers/mysql2ORMController')
 const router = express.Router();
 
 
-<<<<<<< HEAD
-//signup 
-router.post("/signup", async function(req, res) {
-    let connection = await sql.GetConnection();
-    let watchlistArray = await sql.selectSomethingWhere(connection, 'watchlistArray', "users", 'ID', req.body.id);
-    console.log(watchlistArray[0].watchlistArray);
-    connection.end();
-    //lookup in mysql for the user req.body.id and return there watchlist
-
-    res.send("watchlist", { news: data });
-=======
 // /account/
 
 // display home page navebar page
 router.get("/", function(req, res) {
     res.render("DisplayAll")
->>>>>>> 59c3603d9a446bd7489af0e5898f36a8b23460f4
 });
 
 
@@ -52,13 +40,14 @@ router.get("/addnotification", function(req, res) {
     res.render("notificationEdit") //
 });
 
+//notification page for testing
+router.get("/notifications", function(req, res) {
+    res.render("notifications") //
+});
+
 //display all current news 
 router.get("/news", function(req, res) {
-<<<<<<< HEAD
-    res.render("cryptoNews")
-=======
     res.render("news")
->>>>>>> 59c3603d9a446bd7489af0e5898f36a8b23460f4
 });
 
 module.exports = router;
