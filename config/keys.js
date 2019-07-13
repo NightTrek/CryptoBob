5ae6ab183cc3fbde38abe3bc764ebe0a52b9fcf7
Daniels@dual4t.com <daniels@dual4t.com>
2019-07-13 00:15:57 -0700

    changed backend server to run in gcc

--- config/keys.js
+++ config/keys.js
@@ -11,7 +11,8 @@ const keys = {
   key: process.env.Bittrex_key,
   secret: process.env.Bittrex_SECRET,
   ptp: process.env.DataBaseHomeSecret,
-  invalideAttempt:process.env.InvalideBackEndLoginAttempts
+  invalideAttempt:process.env.InvalideBackEndLoginAttempts,
+  mysqlpass:process.env.GCC_mysqlPass
 };
 
 
