0e5a17e39752ae6b2e0b917be852d3a6d31355b7
Garrett Young <garrettyoung510@gmail.com>
2019-07-14 16:40:29 -0700

    test chart file

--- tests/chartstest.js
+++ tests/chartstest.js
@@ -1,99 +1,81 @@
 // dependencies
 const express = require('express');
 const sql = require('../controlers/mysql2ORMController.js');
+const convertMarketData = require('../scripts/convertMarketData.js');
 
-let chart = async function() {
-    try {
-        let connection = await sql.GetConnection();
-        let dataForChart = await sql.selectSomethingWhere(connection, 'dataForChart', 'eth', 'id', '1');
-        console.log(dataForChart[0].dataForChart);
-        data[0].dataPoints.push(dataForChart[0].dataForChart);
-        connection.end();
-    } catch (err) {
-        console.log(err + " code 500 unable to pull market data for token");
-    }
-}
+// window onload function
 
-// display on chart
-window.onload = function() {
-        var chart = new CanvasJS.Chart("chartContainer1", {
-            title: {
-                text: "Multi-Series Line Chart"
-            },
-            // testing the chart 
-            data: [{
-                    type: "line",
-                    dataPoints: [
-                        { x: 10, y: 21 },
-                        { x: 20, y: 25 },
-                        { x: 30, y: 20 },
-                        { x: 40, y: 25 },
-                        { x: 50, y: 27 },
-                        { x: 60, y: 28 },
-                        { x: 70, y: 28 },
-                        { x: 80, y: 24 },
-                        { x: 90, y: 26 }
 
-                    ]
-                },
-                {
-                    type: "line",
-                    dataPoints: [
-                        { x: 10, y: 31 },
-                        { x: 20, y: 35 },
-                        { x: 30, y: 30 },
-                        { x: 40, y: 35 },
-                        { x: 50, y: 35 },
-                        { x: 60, y: 38 },
-                        { x: 70, y: 38 },
-                        { x: 80, y: 34 },
-                        { x: 90, y: 44 }
+$(document).ready(function() {
+    getChartInfo("eth", "usd");
+    getChartInfo("btc", "usd");
+    getChartInfo("xrp", "usd");
+});
 
-                    ]
-                },
-                {
-                    type: "line",
-                    dataPoints: [
-                        { x: 10, y: 45 },
-                        { x: 20, y: 50 },
-                        { x: 30, y: 40 },
-                        { x: 40, y: 45 },
-                        { x: 50, y: 45 },
-                        { x: 60, y: 48 },
-                        { x: 70, y: 43 },
-                        { x: 80, y: 41 },
-                        { x: 90, y: 28 }
+// window.onload = 
+function getChartInfo(ticker, market) {
 
-                    ]
-                },
-                {
-                    type: "line",
-                    dataPoints: [
-                        { x: 10, y: 71 },
-                        { x: 20, y: 55 },
-                        { x: 30, y: 50 },
-                        { x: 40, y: 65 },
-                        { x: 50, y: 95 },
-                        { x: 60, y: 68 },
-                        { x: 70, y: 28 },
-                        { x: 80, y: 34 },
-                        { x: 90, y: 14 }
-
-                    ]
-                }
-            ]
-        });
-
-        chart.render();
-    }
+    let id = ticker;
     // pull data from our database 
-    // $(document).ready(function() {
-    //     $.ajax({
-    //         type: "GET",
-    //         url: "eth.small.csv",
-    //         dataType: "text",
-    //         success: function(data) { processData(data); }
-    //     });
-    // });
+    $.ajax({
+        type: "POST",
+        url: "http://ccce8a57.ngrok.io/api/marketDataforToken",
+        data: {
+            "ticker": ticker,
+            "market": market,
+            "key": "bK9uSGlTGfqFHV0O4unF20sLYD82J0iEHyxPW9qL2bIMNpn2OonKfbUe8Q5JHhHq"
+        },
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(result) {
+            convertMarketData.convertRecentMarketDataIntoGraph(result);
+        }
+    });
+    // push data for chart for specified ticker inputted 
+    // chart to display in chart container1
+    const chart = new CanvasJS.Chart("chartContainer1", {
+        title: {
+            text: `${ticker} Line Chart`
+        },
+        // the chart parameters 
+        data: [{
+                type: "line",
+                // output is defined in the convertrecentmarketdataintograph function
+                dataPoints: output,
+            }
+            // other lines if necessary for future comparisons
+            // {
+            //     type: "line",
+            //     dataPoints: [
+            //         { x: 10, y: 31 },
+            //         { x: 20, y: 35 },
+            //         { x: 30, y: 30 },
+            //         { x: 40, y: 35 },
+            //         { x: 50, y: 35 },
+            //         { x: 60, y: 38 },
+            //         { x: 70, y: 38 },
+            //         { x: 80, y: 34 },
+            //         { x: 90, y: 44 }
 
-// ids= chartContainer1, chartContainer2, chartContainer3
+            //     ]
+            // },
+            // {
+            //     type: "line",
+            //     dataPoints: [
+            //         { x: 10, y: 45 },
+            //         { x: 20, y: 50 },
+            //         { x: 30, y: 40 },
+            //         { x: 40, y: 45 },
+            //         { x: 50, y: 45 },
+            //         { x: 60, y: 48 },
+            //         { x: 70, y: 43 },
+            //         { x: 80, y: 41 },
+            //         { x: 90, y: 28 }
+
+            //     ]
+            // },
+        ]
+    });
+    // renders chart
+    chart.render();
+}
