6f52d52a19ef2d50c10a60110a5a21f1bb3620dd
Daniels@dual4t.com <daniels@dual4t.com>
2019-07-22 10:39:53 -0700

    added notification controller

--- database_server/routes/index.js
+++ database_server/routes/index.js
@@ -73,6 +73,60 @@ router.post("/api/marketDataforToken", async function (req, res) {
 });
 
 
+router.post("/api/currentdata", async function (req, res) {
+  console.log("POST REQ current data");
+  // console.log(req.body.data.ticker)   
+  logger.log({
+      level: 'info',
+      message: `REQ CURRENT MARKET DATA: FROM:${req.body.data.ticker}||${req.body.data.market}`,
+
+    }); 
+  try{
+     let isValid = await bcrypt.compare(req.body.data.key, passwordHash);
+          if(isValid){
+          if(md.key[req.body.data.ticker] !== undefined){
+              let connection = await sql.GetConnection();
+              let tokenData = await sql.SelectAllAndOrderByTmestamp(connection, md.key[req.body.data.ticker], req.body.data.market );
+              if(tokenData){
+                  connection.end();
+                  logger.log({
+                    level: 'info',
+                    message: `SUCCESS found ${req.body.data.ticker} in DB for market: ${req.body.data.market} `,
+            
+                  }); 
+                  res.json(tokenData);
+              }
+              }else{
+                logger.log({
+                  level: 'info',
+                  message: `ERROR 404 data not found in DB: ${req.body.data.ticker} market: ${req.body.data.market} `,
+          
+                }); 
+                  throw "error 404"
+              }
+          }
+          else{
+              console.log("ERROR INVALID API KEY")
+              logger.log({
+                  level: 'info',
+                  message: `INVALID VALIDATION ATTEMPT: ${req.body}`,
+          
+                }); 
+              throw "ERROR INVALID API KEY"
+          }
+  }
+  catch(err){
+    logger.log({
+      level: 'info',
+      message: `ERROR retriving market data: ${req.body.data.ticker}`,
+
+    }); 
+      console.log(err);
+
+  }
+  
+});
+
 //entery page for the app/ login page
 router.get("/api/", function (req, res) {
     logger.log({
